d903e7ea06d2c6c293329615082f8cccbd344817
Mike Chalmers <mikechalmers@gmail.com>
2020-08-18 11:40:00 +0100

    Add new item to main page

--- public/ajax.js
+++ public/ajax.js
@@ -1,3 +1,31 @@
+$('#new-todo-form').submit(function(e) {
+  e.preventDefault();
+
+  var toDoItem = $(this).serialize();
+
+  $.post('/todos', toDoItem, function(data) {
+    $('#todo-list').append(
+      `
+      <li class="list-group-item">
+        <span class="lead">
+          ${data.text}
+        </span>
+        <div class="pull-right">
+          <a href="/todos/${data._id}/edit" class="btn btn-sm btn-warning">Edit</a>
+          <form style="display: inline" method="POST" action="/todos/${data._id}">
+            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+          </form>
+        </div>
+        <div class="clearfix"></div>
+      </li>
+      `
+    )
+    // clear the form after submitting
+    $('#new-todo-form').find('.form-control').val('');
+  });
+
+})
+
 // $.get('/todos', function(data){
 //   console.log(data);
 // });
@@ -9,7 +37,7 @@
 //     console.log(data);
 //   });
 // });
-
+//
 // $('form').submit(function(e) {
 //   e.preventDefault();
 //   var formData = $(this).serialize();
@@ -23,15 +51,15 @@
 //     }
 //   });
 // });
-
-$('form').submit(function(e) {
-  e.preventDefault();
-  var formAction = $(this).attr('action');
-  $.ajax({
-    url: formAction,
-    type: 'DELETE',
-    success: function(data) {
-      console.log(data);
-    }
-  });
-});
+//
+// $('form').submit(function(e) {
+//   e.preventDefault();
+//   var formAction = $(this).attr('action');
+//   $.ajax({
+//     url: formAction,
+//     type: 'DELETE',
+//     success: function(data) {
+//       console.log(data);
+//     }
+//   });
+// });
